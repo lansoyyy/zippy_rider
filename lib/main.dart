2a4bbd85afa4358d9ece33ff4a619eec78fd0565
Lans12345 <olanalans12345@gmail.com>
2024-09-17 18:18:00 +0800

    renamed

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Zippy',
+      title: 'Zippy Rider',
       home: OnboardingOne(),
     );
   }
