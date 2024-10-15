2b55c5e12b4027216dd0f32a3655880583e0295b
Lans12345 <olanalans12345@gmail.com>
2024-10-15 18:30:42 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:zippy/firebase_options.dart';
 import 'package:zippy/screens/auth/landing_screen.dart';
 import 'package:zippy/screens/home_screen.dart';
 import 'package:zippy/screens/onboarding_screens/onboarding_one.dart';
 import 'package:zippy/screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'zippy-3c566',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
